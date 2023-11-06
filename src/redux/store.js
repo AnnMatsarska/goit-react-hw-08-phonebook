10b820bf208df801b582e01f297a43b8fffd20e9
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-11-06 14:01:37 +0200

    add: filter slice, api client, auth operations and thunk

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { filterReducer } from './filter/slice';
+
+const authConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
